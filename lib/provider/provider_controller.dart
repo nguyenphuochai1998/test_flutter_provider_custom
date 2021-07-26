d6ecc3d63c9fb2ca282a20ad5c59824880c7674f
nguyenphuochai1998 <haito1998@gmail.com>
2021-07-26 12:00:50 +0700

    update_comment

--- lib/provider/provider_controller.dart
+++ lib/provider/provider_controller.dart
@@ -14,27 +14,25 @@ class ProviderController extends StatefulWidget {
       return result;
     throw FlutterError.fromParts(<DiagnosticsNode>[
       ErrorSummary(
-        'Scaffold.of() called with a context that does not contain a Scaffold.',
+        'Scaffold.of() called with a context that does not contain a ProviderController.',
       ),
       ErrorDescription(
-        'No Scaffold ancestor could be found starting from the context that was passed to Scaffold.of(). '
+        'No Scaffold ancestor could be found starting from the context that was passed to ProviderController.of(). '
             'This usually happens when the context provided is from the same StatefulWidget as that '
             'whose build function actually creates the Scaffold widget being sought.',
       ),
       ErrorHint(
-        'There are several ways to avoid this problem. The simplest is to use a Builder to get a '
-            'context that is "under" the Scaffold. For an example of this, please see the '
-            'documentation for Scaffold.of():\n'
-            '  https://api.flutter.dev/flutter/material/Scaffold/of.html',
+          'There are several ways to avoid this problem. The simplest is to use a Builder to get a '
+              'context that is "under" the ProviderController. For an example of this, please see the '
       ),
       ErrorHint(
         'A more efficient solution is to split your build function into several widgets. This '
-            'introduces a new context from which you can obtain the Scaffold. In this solution, '
-            'you would have an outer widget that creates the Scaffold populated by instances of '
-            'your new inner widgets, and then in these inner widgets you would use Scaffold.of().\n'
-            'A less elegant but more expedient solution is assign a GlobalKey to the Scaffold, '
-            'then use the key.currentState property to obtain the ScaffoldState rather than '
-            'using the Scaffold.of() function.',
+            'introduces a new context from which you can obtain the ProviderController. In this solution, '
+            'you would have an outer widget that creates the ProviderController populated by instances of '
+            'your new inner widgets, and then in these inner widgets you would use ProviderController.of().\n'
+            'A less elegant but more expedient solution is assign a GlobalKey to the ProviderController, '
+            'then use the key.currentState property to obtain the ProviderControllerState rather than '
+            'using the ProviderController.of() function.',
       ),
       context.describeElement('The context used was'),
     ]);
